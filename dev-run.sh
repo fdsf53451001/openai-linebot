dfb26472afd50d6ab73c476c29b1e0da38447f7b
fdsf53451001 <fdsf53451001@gmail.com>
2023-10-13 14:00:17 +0800

    bug fix

--- dev-run.sh
+++ dev-run.sh
@@ -1,6 +1,6 @@
 sudo docker-compose down
 # git stash
-git pull
+# git pull
 sudo docker build -t openai-linebot . -f ./docker/main/Dockerfile --no-cache
 sudo docker build -t openai-linebot-grafana . -f ./docker/grafana/Dockerfile --no-cache
 sudo docker-compose up -d
